55c9ca5d38d6568323e0cdc1bdc3bcc20c99bcea
Fernando Montano <fernando.montano41@gmail.com>
2022-09-08 09:44:36 -0600

    Finished drawing app, need to comment.

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useState, useLayoutEffect, useRef } from "react";
 import Colors from "./Colors";
+import { useDraw } from "./Hooks";
 
 export default function Canvas() {
+  const [color, setColor] = useState();
+
+  const changeColor = (props) => {
+    setColor(props);
+  };
+
+  const canvRef = useDraw(onDraw);
+
+  function onDraw(ctx, point, prev) {
+    drawLine(prev, point, ctx, color, 2);
+  }
+
+  function drawLine(start, end, ctx, color, width) {
+    start = start ?? end;
+    ctx.beginPath();
+    ctx.lineWidth = width;
+    ctx.strokeStyle = color;
+    ctx.moveTo(start.x, start.y);
+    ctx.lineTo(end.x, end.y);
+    ctx.stroke();
+
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(start.x, start.y, 0.7, 0, 2 * Math.PI, false);
+    ctx.fill();
+  }
+
   return (
     <section className="vh-100 d-flex justify-content-center align-items-center">
-      <canvas className="w-75 h-75 bg-light border-1"> </canvas>
-      <Colors />
+      <canvas
+        className="bg-light border rounded"
+        ref={canvRef}
+        width="1000px"
+        height="500px"
+      />
+      <Colors selectedColor={changeColor} />
     </section>
   );
 }
--- src/components/Colors.js
+++ src/components/Colors.js
@@ -3,25 +3,38 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-export default function Colors() {
+
+export default function Colors({ selectedColor }) {
+  const handleClick = () => {
+    // handle click is the event that changes the selected color from Canvas component -🙂
+    window.onclick = (e) => {
+      // e.target => selected div.
+      selectedColor(window.getComputedStyle(e.target).backgroundColor);
+    };
+  };
+
   return (
     <div>
-      <Container>
-        <Row className="m-1">
-          <Col className="square square-lg bg-danger text-danger p-4 m-1"></Col>
-          <Col className="square square-lg bg-warning text-success p-4 m-1"></Col>
+      <Container >
+        <Row className="m-1" onClick={handleClick}>
+          <Col className="square rounded square-lg bg-danger text-danger p-4 m-1"></Col>
+          <Col className="square rounded square-lg bg-warning text-success p-4 m-1 "></Col>
         </Row>
-        <Row className="m-1">
-          <Col className="square square-lg bg-success p-4 m-1"></Col>
-          <Col className="square square-lg bg-info p-4 m-1"></Col>
+        <Row className="m-1" onClick={handleClick}>
+          <Col className="square rounded square-lg bg-success p-4 m-1"></Col>
+          <Col className="square rounded square-lg bg-info p-4 m-1"></Col>
         </Row>
-        <Row className="m-1">
-          <Col className="square square-lg bg-secondary p-4 m-1"></Col>
-          <Col className="square square-lg bg-dark p-4 m-1"></Col>
+        <Row className="m-1" onClick={handleClick}>
+          <Col className="square rounded square-lg bg-secondary p-4 m-1"></Col>
+          <Col className="square rounded square-lg bg-dark p-4 m-1"></Col>
         </Row>
-        <Row className="m-1">
-          <Col className="square square-lg bg-$indigo p-4 m-1"></Col>
-          <Col className="square square-lg bg-dark p-4 m-1"></Col>
+        <Row className="m-1" onClick={handleClick}>
+          <Col className="square rounded square-lg color-indigo p-4 m-1"></Col>
+          <Col className="square rounded square-lg bg-light p-4 m-1"></Col>
+        </Row>
+        <Row className="m-1" onClick={handleClick}>
+          <Col className="square rounded square-lg bg-$indigo p-4 m-1"></Col>
+          <Col className="square rounded square-lg bg-dark p-4 m-1"></Col>
         </Row>
       </Container>
     </div>
