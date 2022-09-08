750bcdb8e7ac6eb5508042256ec7949e37c16ba7
Andrea Velásquez <67344634+andreavvel@users.noreply.github.com>
2022-09-08 14:14:29 -0600

    Adding custom colors 🌈

--- src/components/Colors.js
+++ src/components/Colors.js
@@ -2,6 +2,7 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import "./css/Colors.css"
 
 export default function Colors({ selectedColor }) {
   // returns the colors with each row containing properties to select the desired color. 👀
@@ -16,7 +17,7 @@ export default function Colors({ selectedColor }) {
         <Row className="m-1">
           <Col className="square rounded square-lg bg-danger text-danger p-4 m-1"></Col>
           <Col className="square rounded square-lg bg-warning text-success p-4 m-1 "></Col>
-        </Row>
+      </Row>
         <Row className="m-1">
           <Col className="square rounded square-lg bg-success p-4 m-1"></Col>
           <Col className="square rounded square-lg bg-info p-4 m-1"></Col>
@@ -26,12 +27,16 @@ export default function Colors({ selectedColor }) {
           <Col className="square rounded square-lg bg-dark p-4 m-1"></Col>
         </Row>
         <Row className="m-1">
-          <Col className="square rounded square-lg color-indigo p-4 m-1"></Col>
-          <Col className="square rounded square-lg bg-light p-4 m-1"></Col>
+          <Col className="square rounded square-lg deeppink p-4 m-1"></Col>
+          <Col className="square rounded square-lg indigo p-4 m-1"></Col>
         </Row>
         <Row className="m-1">
-          <Col className="square rounded square-lg bg-$indigo p-4 m-1"></Col>
-          <Col className="square rounded square-lg bg-dark p-4 m-1"></Col>
+          <Col className="square rounded square-lg springgreen p-4 m-1"></Col>
+          <Col className="square rounded square-lg navy p-4 m-1"></Col>
+        </Row>
+        <Row className="m-1">
+          <Col className="square rounded square-lg orange p-4 m-1"></Col>
+          <Col className="square rounded square-lg bg-light p-4 m-1"></Col>
         </Row>
       </Container>
     </div>
